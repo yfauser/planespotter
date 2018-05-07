061b30e4b4980d2cadf2ed3b22b578c618691552
howardyoo <32691630+howardyoo@users.noreply.github.com>
2018-05-07 11:53:11 -0700

    modified user and password parameters
    some mysql system does NOT (somehow) recognize password when shortened format is used (e.g. -p password). Using --user and --password will remove this chance of error.

--- db-install/create-planespotter-db.sh
+++ db-install/create-planespotter-db.sh
@@ -4,5 +4,5 @@ cd ~/planespotter/db-install/
 wget http://registry.faa.gov/database/ReleasableAircraft.zip
 unzip ReleasableAircraft.zip
 rm ReleasableAircraft.zip DEALER.txt DEREG.txt DOCINDEX.txt ENGINE.txt RESERVED.txt
-mysql -u root -p < create-planespotter-db.sql
+mysql --user=root --password=$MYSQL_ROOT_PASSWORD < create-planespotter-db.sql
 rm MASTER.txt ACFTREF.txt
